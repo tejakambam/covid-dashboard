4d31f24c52eba6a542dd6f7775744cfa49f3a2f4
tejakambam <teja@appliedsyntax.io>
2021-04-04 19:59:42 +0530

    Dashboard with covid 19 india data alonn with states, Login and register

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,129 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+    private userSubject: BehaviorSubject<User>;
+    public user: Observable<User>;
+
+    data1
+    state
+    district
+    BannerData: BehaviorSubject<any> = new BehaviorSubject('')
+    districtdata: Subject<any> = new Subject<any>();
+  
+    url_statewise = 'https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise'
+    url_dailycases = 'https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise/history'
+    ulr_districtwise = "https://api.covid19india.org/state_district_wise.json"
+    url_banner = "https://api.covid19india.org/website_data.json"
+
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+        this.user = this.userSubject.asObservable();
+    }
+
+
+    ngOnInit(): void {
+
+        this.getDataStateWise()
+      }
+    
+      getBanners(): Observable<any> {
+        return this.http.get(this.url_banner)
+      }
+    
+      getDataStateWise(): Observable<any> {
+        return this.http.get(this.url_statewise)
+      }
+    
+     getDailyCaseStatus(): Observable<any> {
+        return this.http.get(this.url_dailycases)
+      }
+    
+      getState(state) {
+        this.state = state
+      }
+    
+      getDataDistrictWise(state) {
+        this.http.get(this.ulr_districtwise).subscribe(data => {
+          this.data1 = data
+        //  console.log(this.data1)
+       //   console.log(this.data1[state])
+          this.district = this.data1[state].districtData
+        //  console.log(state)
+          this.districtdata.next(this.district)
+        }
+        )
+      }
+
+
+    public get userValue(): User {
+        return this.userSubject.value;
+    }
+
+    login(username, password) {
+        return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('user', JSON.stringify(user));
+                this.userSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+        this.router.navigate(['/account/login']);
+    }
+
+    register(user: User) {
+        return this.http.post(`${environment.apiUrl}/users/register`, user);
+    }
+
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
+
+    getById(id: string) {
+        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+    }
+
+    update(id, params) {
+        return this.http.put(`${environment.apiUrl}/users/${id}`, params)
+            .pipe(map(x => {
+                // update stored user if the logged in user updated their own record
+                if (id == this.userValue.id) {
+                    // update local storage
+                    const user = { ...this.userValue, ...params };
+                    localStorage.setItem('user', JSON.stringify(user));
+
+                    // publish updated user to subscribers
+                    this.userSubject.next(user);
+                }
+                return x;
+            }));
+    }
+
+    delete(id: string) {
+        return this.http.delete(`${environment.apiUrl}/users/${id}`)
+            .pipe(map(x => {
+                // auto logout if the logged in user deleted their own record
+                if (id == this.userValue.id) {
+                    this.logout();
+                }
+                return x;
+            }));
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { MaterialModule } from './material.module';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+;
+import { DistrictComponent } from './district/district.component'
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        CommonModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+,
+        DistrictComponent    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
